a9396259cad817801af2bcda4d3225f0a231d750
Nitya Narasimhan <n.narasimhan@gmail.com>
2022-12-16 12:57:58 -0500

    fix: Naming typo

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -35,7 +35,7 @@ const CoreServices = [
     ),
   },
   {
-    title: 'Azure RedHat Open Shift',
+    title: 'Azure RedHat OpenShift',
     svgpath: 'img/svg/azure-openshift.svg',
     link: "https://azure.microsoft.com/services/openshift/",
     description: (
