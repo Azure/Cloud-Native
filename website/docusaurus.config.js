074ac9d07ccabae38e6157dc628395743b110624
v-jstruve <129466250+v-jstruve@users.noreply.github.com>
2023-09-12 11:15:20 -0700

    link and metadata updates

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -102,7 +102,7 @@ const config = {
             position: "right",
             items: [
               {
-                label: "🍁 | FallForIA 2023",
+                label: "🍂 | FallForIA 2023",
                 href: "/Fall-For-IA/",
               },
               {
@@ -227,7 +227,7 @@ const config = {
         {
           name: "twitter:image",
           content:
-            "https://azure.github.io/Cloud-Native/assets/ideal-img/hero-banner.e0a8d29.1030.png",
+            "https://github.com/Azure/Cloud-Native/blob/main/website/static/img/ogImage.png",
         },
         {
           name: "twitter:card",
