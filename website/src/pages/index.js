eb1e1d31e4dcec35142cd10aecaebcb6efeb857e
Josh Struve <142850611+jstruve-catalysis@users.noreply.github.com>
2023-10-02 14:48:28 -0700

    "Build IA" branding update

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/Fall-For-IA/"
+            to="/Build-IA/"
           >
             Build Intelligent Apps
           </Link>
